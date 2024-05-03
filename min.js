c9417b7dc40ee0e8195d14d233937522ab4c2422
awaa16 <syalwamadani16@gmail.com>
2024-05-03 07:48:14 +0700

    menambah import

---
+++ min.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
